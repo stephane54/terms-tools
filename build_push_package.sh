9bae14ede9786fc2aef9133805a39b999720229b
schneist <stephane.schneider@inist.fr>
2024-08-27 16:25:50 +0200

    maj package

--- build_push_package.sh
+++ build_push_package.sh
@@ -32,9 +32,9 @@ echo "---- PUSH distrib ........................................................
 git add .
 git commit -m "maj package"
 tag=$(cat tag.txt)
-if [[ -n $1 ]]; then
+if [[ -n "$1" ]]; then
 
-    if [[ $1 == "--tagged" ]]; then
+    if [[ "$1" == "--tagged" ]]; then
       echo "tag version with : $tag"
       git tag -d $tag  && git push --delete origin $tag
       git tag $tag &&  git push http://schneist:merlin@vxgit.intra.inist.fr:60000/git/schneist/terms_tools.git  $tag
@@ -47,10 +47,10 @@ else
   git push http://schneist:merlin@vxgit.intra.inist.fr:60000/git/schneist/terms_tools.git --all
 fi
 
-if [[ -n $2 ]]; then
+if [[ -n "$2" ]]; then
     # deploie en local via git
 
-    if [[ $1 == "--deploy" ]]; then
+    if [[ "$1" == "--deploy" ]]; then
         echo "---- DEPLOY ........................................................."
         pip3 uninstall -y terms_tools
         pip3 install --no-cache-dir git+http://vxgit.intra.inist.fr:60000/git/schneist/terms_tools.git@${tag}#egg=terms_tools
