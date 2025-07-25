a11c41b6584a275f11cca4a271fc7b1039494cbd
stephane54 <stephane.schneider@inist.fr>
2025-07-25 16:37:17 +0200

    maj package

--- setup.py
+++ setup.py
@@ -13,15 +13,15 @@ setup(
     version=version,
     zip_safe=False,
     author="stephane schneider",
-    include_package_data=False,
+    include_package_data=True,
     package_data={"": ["*.jsonl", "*.tsv", "*.json", "*.txt", "*.ini"]}, # "nlptools"
-    exclude={'nlptools.models.*'},
+    excluded={'models.*'},
     author_email="stephane.schneider@inist.fr",
     maintainer="stephane",
     maintainer_email="stephane.schneider@inist.fr",
     keywords="nlp scientific package Python for computationnal terminology on Loterre data",
     classifiers=["Topic :: natural langage processsing", "Topic :: Documentation"],
-    packages=find_packages(exclude=['nlptools.models.*']),
+    packages=find_packages(excluded=['models.*']),
     entry_points={
         "console_scripts": ["terms_tools = nlptools.terms_toolsCLI:console_scripts_main", "loterre_tag = nlptools.term_taggerCLI:console_scripts_main" ]
     },
