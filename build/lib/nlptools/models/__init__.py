f2a13ac81199805d97c64a68a48f5b2b00633798
stephane54 <stephane.schneider@inist.fr>
2025-07-25 23:08:55 +0200

    maj package

--- build/lib/nlptools/models/__init__.py
+++ build/lib/nlptools/models/__init__.py
@@ -14,7 +14,7 @@ __all__ = [
 ]
 
 # Version installation download
-modele_init_en = "en_core_web_trf"
-modele_init_fr = "fr_dep_news_trf"
+modele_init_en = "en_core_web_trf-3.8.0-py3-none-any/en_core_web_trf"
+modele_init_fr = "fr_dep_news_trf-3.8.0-py3-none-any/fr_dep_news_trf"
 
 
