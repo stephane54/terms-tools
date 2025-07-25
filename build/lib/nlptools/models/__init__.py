a389e4cabdcaf6d6aecc92ff047ac8472a773751
stephane54 <stephane.schneider@inist.fr>
2025-07-25 23:27:39 +0200

    maj package

--- build/lib/nlptools/models/__init__.py
+++ build/lib/nlptools/models/__init__.py
@@ -14,6 +14,7 @@ __all__ = [
 ]
 
 # Version installation download
+# MODEL_DIR="/home/schneist/app/terms_tools/terms_tools/nlptools/models"
  
 modele_init_en = os.path.join(os.environ.get('MODEL_DIR'),"/en_core_web_trf-3.8.0-py3-none-any/en_core_web_trf/en_core_web_trf-3.8.0/")
 modele_init_fr = os.path.join(os.environ.get('MODEL_DIR'),"/fr_dep_news_trf-3.8.0-py3-none-any/fr_dep_news_trf/fr_dep_news_trf-3.8.0/")
