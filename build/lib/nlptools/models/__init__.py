a0fc0fc5c8b992876a5ab037c08d9bbd618ed061
stephane54 <stephane.schneider@inist.fr>
2025-09-01 16:21:21 +0200

    maj package

--- build/lib/nlptools/models/__init__.py
+++ build/lib/nlptools/models/__init__.py
@@ -1,16 +1,14 @@
 import os
 
-# Version installation download 
-modele_init_en = os.path.join(os.environ.get('MODEL_DIR'),"en_core_web_trf-3.8.0-py3-none-any/en_core_web_trf","en_core_web_trf-3.8.0")
-modele_init_fr = os.path.join(os.environ.get('MODEL_DIR'),"fr_dep_news_trf-3.8.0-py3-none-any/fr_dep_news_trf","fr_dep_news_trf-3.8.0")
+# Version pre-installation 
+#modele_init_en = os.path.join(os.environ.get('MODEL_DIR'),"en_core_web_trf-3.8.0-py3-none-any/en_core_web_trf","en_core_web_trf-3.8.0")
+#modele_init_fr = os.path.join(os.environ.get('MODEL_DIR'),"fr_dep_news_trf-3.8.0-py3-none-any/fr_dep_news_trf","fr_dep_news_trf-3.8.0")
 
-'''
-# Version installation locale
-# nlptools/models/en_core_web_trf-3.8.0-py3-none-any/en_core_web_trf/en_core_web_trf-3.8.0/
-modele_init_en = os.path.join(os.path.dirname(__file__) ,"en_core_web_trf-3.8.0-py3-none-any/en_core_web_trf", "en_core_web_trf-3.8.0")
-# nlptools/models/fr_dep_news_trf-3.8.0-py3-none-any/fr_dep_news_trf/fr_dep_news_trf-3.8.0/
-modele_init_fr = os.path.join(os.path.dirname(__file__) ,"fr_dep_news_trf-3.8.0-py3-none-any/fr_dep_news_trf", "fr_dep_news_trf-3.8.0")
-'''
+#version post install pdt deploiement
+modele_init_en = "en_core_web_trf"
+modele_init_fr = "fr_dep_news_trf"
+
+
 __all__ = [
     "modele_init_fr",
     "modele_init_en"
