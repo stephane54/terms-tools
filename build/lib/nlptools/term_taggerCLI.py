7dfbb67ac7dea79325a6cfea67523bcbea70e170
stephane54 <stephane.schneider@inist.fr>
2025-09-10 08:19:47 +0200

    maj package

--- build/lib/nlptools/term_taggerCLI.py
+++ build/lib/nlptools/term_taggerCLI.py
@@ -63,7 +63,7 @@ __contact__ = "stephane.schneider@inist.fr"
 # --------------------
 log = "term-tagger.log"
 core = cpu_count()
-docjer silent = False  # niveau de description dans les log
+silent = False  # niveau de description dans les log
 
 @plac.annotations(
     corpus=("Path to corpus file", "option", "corpus", str),
@@ -138,7 +138,7 @@ def main(corpus,matcher_dico, output, add_stop, prefix, format, input, language=
     keywords_dict = os.path.join(dico_path , matcher_dico)
     
     if matcher_dico: 
-        #check name file       
+        #check name file     
         if not (os.path.isfile(keywords_dict)):
             raise ValueError(keywords_dict)
         
@@ -146,7 +146,7 @@ def main(corpus,matcher_dico, output, add_stop, prefix, format, input, language=
     # config Spacy pipeline must be compatible with the resource format (lemma,lower,raw)
     if norm == "lemma": # fr
         nlp = spacy.load(modele_init_fr, disable=["ner", "parser", "textcat"])
-        nlp.max_length = 1000000 # or higher
+        nlp.max_length = 1000000  # or higher
 
     elif norm == "lower":
         nlp = spacy.blank(language)
